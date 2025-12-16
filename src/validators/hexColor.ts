10adcfb0394e4ee3d8e2971a7673112076772669
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-12-16 14:54:48 +0800

    Update hexColor validation schema.

--- src/validators/hexColor.ts
+++ src/validators/hexColor.ts
@@ -1,16 +1,25 @@
 import z from 'zod';
 import { isValidHexColor, normalizeHexColor } from './utils.js';
 
-export const hexColorValidator = z.string().transform((color, ctx) => {
-  if (!isValidHexColor(color)) {
-    ctx.addIssue({
-      code: 'custom',
-      message: "Invalid color: must be #rgb, #rrggbb, #rrggbbaa, or 'transparent'.",
-      input: color,
-    });
+export const hexColorValidator = z.union([
+  z.string().transform((color, ctx) => {
+    if (!isValidHexColor(color)) {
+      ctx.addIssue({
+        code: 'custom',
+        message: "Invalid color: must be #rgb, #rrggbb, #rrggbbaa, or 'transparent'.",
+        input: color,
+      });
 
-    return z.NEVER;
-  }
+      return z.NEVER;
+    }
 
-  return normalizeHexColor(color);
-});
+    return normalizeHexColor(color);
+  }),
+
+  z.object({
+    r: z.number().int().min(0).max(255),
+    g: z.number().int().min(0).max(255),
+    b: z.number().int().min(0).max(255),
+    a: z.number().int().min(0).max(1),
+  }),
+]);
