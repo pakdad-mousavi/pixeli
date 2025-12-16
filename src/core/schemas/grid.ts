3a0c689703ae89310aabfb6d03faf4532eac25c6
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-12-16 17:07:34 +0800

    Create the core grid schema.

---
+++ src/core/schemas/grid.ts
@@ -0,0 +1,31 @@
+import z from 'zod';
+import { VALIDATORS } from '../../validators/index.js';
+
+export const gridSchema = z
+  .strictObject({
+    recursive: VALIDATORS.recursive.default(false),
+    shuffle: VALIDATORS.shuffle.default(false),
+    cornerRadius: VALIDATORS.cornerRadius.default(0),
+    gap: VALIDATORS.gap.default(50),
+    canvasColor: VALIDATORS.canvasColor.prefault('#fff'),
+    output: VALIDATORS.output.default('./pixeli.png'),
+    aspectRatio: VALIDATORS.aspectRatio.prefault('1:1'),
+    imageWidth: VALIDATORS.imageWidth.optional(),
+    columns: VALIDATORS.columns.default(4),
+    caption: VALIDATORS.caption.default(false),
+    captions: VALIDATORS.captions.nonempty().optional(),
+    captionColor: VALIDATORS.captionColor.prefault('#000'),
+    maxCaptionSize: VALIDATORS.maxCaptionSize.default(100),
+  })
+  .superRefine((opts, ctx) => {
+    // Ensure captions are given if caption is set to true
+    if (opts.caption && (!opts.captions || opts.captions.length === 0)) {
+      ctx.addIssue({
+        code: 'custom',
+        message: 'Caption texts must be provided.',
+        path: ['captions'],
+      });
+    }
+  });
+
+export type GridSchema = typeof gridSchema;
