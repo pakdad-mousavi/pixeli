46595708ff2a14d3388f5e1f417ba6be563a050e
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-12-16 17:04:19 +0800

    Add the aspect ratio validator.

---
+++ src/validators/aspectRatio.ts
@@ -0,0 +1,18 @@
+import z from 'zod';
+import { parseAspectRatio } from './utils.js';
+
+export const aspectRatioValidator = z.coerce.string().transform((ratio, ctx) => {
+  const result = parseAspectRatio(ratio);
+
+  // Validate aspect ratio
+  if (!result) {
+    ctx.addIssue({
+      code: 'custom',
+      message: 'Invalid aspect ratio: Examples of valid ratios include 16/9, 2:3, 1x2, 1.77.',
+      input: ratio,
+    });
+    return z.NEVER;
+  }
+
+  return result;
+});
