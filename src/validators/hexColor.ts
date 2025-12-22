f4fbf7731ca23f5f5b26ba3f9e81acf5206de6e9
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2025-12-22 17:55:28 +0800

    Add a general error message for hexColorValidator union.

--- src/validators/hexColor.ts
+++ src/validators/hexColor.ts
@@ -2,25 +2,37 @@ import z from 'zod';
 import { isValidHexColor } from '../core/helpers.js';
 import { hexToRgba } from '../core/utils/hexToRgba.js';
 
-export const hexColorValidator = z.union([
-  z.string().transform((color, ctx) => {
-    if (!isValidHexColor(color)) {
-      ctx.addIssue({
-        code: 'custom',
-        message: "Invalid color: must be #rgb, #rrggbb, #rrggbbaa, or 'transparent'.",
-        input: color,
-      });
+export const hexColorValidator = z.union(
+  [
+    z.string().transform((color, ctx) => {
+      // Handle transparency
+      if (color === 'transparent') {
+        return hexToRgba('#00000000');
+      }
 
-      return z.NEVER;
-    }
+      // Handle hex values
+      if (!isValidHexColor(color)) {
+        ctx.addIssue({
+          code: 'custom',
+          message: "Invalid color: must be 'transparent', #rgb, #rrggbb, #rrggbbaa, or object of {r, g, b, a}.",
+          input: color,
+        });
 
-    return hexToRgba(color);
-  }),
+        return z.NEVER;
+      }
 
-  z.object({
-    r: z.number().int().min(0).max(255),
-    g: z.number().int().min(0).max(255),
-    b: z.number().int().min(0).max(255),
-    alpha: z.number().min(0).max(1),
-  }),
-]);
+      return hexToRgba(color);
+    }),
+
+    z.object(
+      {
+        r: z.number().int().min(0).max(255),
+        g: z.number().int().min(0).max(255),
+        b: z.number().int().min(0).max(255),
+        alpha: z.number().min(0).max(1),
+      },
+      "Invalid color: must be 'transparent', #rgb, #rrggbb, #rrggbbaa, or object of {r, g, b, a}."
+    ),
+  ],
+  "Invalid color: must be 'transparent', #rgb, #rrggbb, #rrggbbaa, or object of {r, g, b, a}."
+);
