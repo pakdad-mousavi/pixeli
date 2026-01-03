6cfc93b7a6dc4a30869736c89e2c89f2de6d4db5
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-03 15:20:09 +0800

    Implement the applyComposites mergeStep.

---
+++ src/core/merges/shared-steps/applyComposites.ts
@@ -0,0 +1,23 @@
+import { MESSAGES } from '../../../cli/modules/messages.js';
+import { MergeError } from '../../mergeError.js';
+import type { MergeStep } from '../../pipeline/mergePipeline.js';
+
+export const applyComposites: MergeStep<any, any> = async (context, _options, _onProgress) => {
+  // Ensure canvas exists
+  if (!context.canvas) {
+    throw new MergeError(MESSAGES.ERROR.INTERNAL.message, {
+      type: 'internal',
+      cause: 'cannot apply composites on undefined canvas',
+    });
+  }
+
+  // Create final grid
+  try {
+    context.canvas = context.canvas.composite(context.composites);
+  } catch (err) {
+    throw new MergeError(MESSAGES.ERROR.INTERNAL.message, {
+      type: 'internal',
+      cause: err,
+    });
+  }
+};
