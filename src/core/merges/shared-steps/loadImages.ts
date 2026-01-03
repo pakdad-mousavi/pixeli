8b987fad39fa603cd9c2f91d8e9bd73b78012b32
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-03 15:11:14 +0800

    Implement the loadImages mergeStep.

---
+++ src/core/merges/shared-steps/loadImages.ts
@@ -0,0 +1,29 @@
+import sharp from 'sharp';
+import type { MergeStep } from '../../pipeline/mergePipeline.js';
+import { isActualImage } from '../../utils/images/isActualImage.js';
+import { MergeError } from '../../mergeError.js';
+
+export const loadImages: MergeStep<any, any> = async (context, _options, _onProgress) => {
+  const images: sharp.Sharp[] = [];
+  for (let i = 0; i < context.inputs.length; i++) {
+    // Ensure image is valid
+    const input = context.inputs[i]!;
+    const { isImage, reason } = await isActualImage(input);
+
+    if (!isImage) {
+      throw new MergeError(`Invalid image input at index ${i}`, {
+        type: 'validation',
+        cause: reason,
+      });
+    }
+
+    images.push(sharp(input));
+  }
+
+  // Ensure there's at least one image
+  if (images.length <= 0) {
+    throw new MergeError('No images provided to merge', { type: 'validation' });
+  }
+
+  context.images = images;
+};
