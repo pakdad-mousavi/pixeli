e3d9622d3fda849d9f93c0f075f51e8a401187fe
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-03 23:23:08 +0800

    Update imageWidth description for GridMergeOptions.

--- src/core/merges/types.ts
+++ src/core/merges/types.ts
@@ -46,7 +46,7 @@ export interface GridMergeOptions {
   /** Grid aspect ratio (e.g. "16:9" or 1.777). */
   aspectRatio?: string | number;
 
-  /** Width of each image cell in pixels, uses the smallest image width if undefined. */
+  /** Width of each image cell in pixels, uses the median image width if undefined. */
   imageWidth?: number | undefined;
 
   /** Number of columns in the grid. */
