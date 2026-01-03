1e98b122c06395131bbff568ed0d8e0c604086e3
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-03 15:18:03 +0800

    Implement the calculateCanvasDimensions mergeStep.

---
+++ src/core/merges/grid/steps/calculateImageDimensions.ts
@@ -0,0 +1,31 @@
+import type { MergeStep } from '../../../pipeline/mergePipeline.js';
+import type { GridState } from '../index.js';
+
+import { MESSAGES } from '../../../../cli/modules/messages.js';
+import { MergeError } from '../../../mergeError.js';
+import { getImageWidths } from '../../../utils/images/getImageWidths.js';
+import { trimmedMedian } from '../../../utils/math/trimmedMedian.js';
+
+interface calculateImageDimensionsOptions {
+  imageWidth?: number | undefined;
+  aspectRatio: number;
+}
+
+export const calculateImageDimensions: MergeStep<calculateImageDimensionsOptions, GridState> = async (
+  context,
+  options,
+  _onProgress
+) => {
+  // Calculate image width
+  const width = options.imageWidth || trimmedMedian(await getImageWidths(context.images));
+  if (width === null) {
+    throw new MergeError(MESSAGES.ERROR.INTERNAL.message, { type: 'internal', cause: 'trimmedMedian failed' });
+  }
+
+  // Calculate image height
+  const height = Math.floor(width / options.aspectRatio);
+
+  // Assign to context
+  context.state.imageWidth = width;
+  context.state.imageHeight = height;
+};
