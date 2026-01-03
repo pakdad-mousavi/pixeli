5c5bb6eb439f977feaab4b2712aacfdf3af871c3
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-03 15:22:53 +0800

    Implement the exportCanvas mergeStep.

---
+++ src/core/merges/shared-steps/exportCanvas.ts
@@ -0,0 +1,38 @@
+import { MESSAGES } from '../../../cli/modules/messages.js';
+import type { SupportedOutputFormat } from '../../helpers.js';
+import { MergeError } from '../../mergeError.js';
+import type { MergeStep } from '../../pipeline/mergePipeline.js';
+
+interface ExportCanvasOptions {
+  format: SupportedOutputFormat;
+}
+
+export const exportCanvas: MergeStep<ExportCanvasOptions, any> = async (context, options, _onProgress) => {
+  // Ensure canvas exists
+  if (!context.canvas) {
+    throw new MergeError(MESSAGES.ERROR.INTERNAL.message, {
+      type: 'internal',
+      cause: 'cannot apply composites on undefined canvas',
+    });
+  }
+
+  try {
+    return await context.canvas.toFormat(options.format).toBuffer();
+  } catch (err) {
+    // Assuming ALL sharp errors are instances of the Error object
+    const sharpError = err as Error;
+
+    // SPECIFIC SHARP ERROR
+    // occurs when trying to create a buffer that exceeds the limits of the current image format
+    if (sharpError.message.includes('pixel limit') || sharpError.message.includes('Processed image is too large')) {
+      const errText = `Error: image to large for '${options.format}' format, try a format that allows larger images`;
+      throw new MergeError(errText, { type: 'image' });
+    }
+
+    // Other sharp errors
+    throw new MergeError(MESSAGES.ERROR.INTERNAL.message, {
+      type: 'internal',
+      cause: err,
+    });
+  }
+};
