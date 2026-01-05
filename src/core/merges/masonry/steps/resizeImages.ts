ed05f2bb45a0ef10fb1a0233175c0e6ec589269f
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-05 16:14:35 +0800

    Implement the resizeImages merge step.

---
+++ src/core/merges/masonry/steps/resizeImages.ts
@@ -0,0 +1,21 @@
+import { requireState } from '../../../pipeline/guards.js';
+import type { MergeStep } from '../../../pipeline/mergePipeline.js';
+import type { MasonryState } from '../index.js';
+
+import { scaleImages } from '../../../utils/images/scaleImages.js';
+
+interface ResizeImagesImagesOptions {
+  flow: 'horizontal' | 'vertical';
+}
+
+export const resizeImages: MergeStep<ResizeImagesImagesOptions, MasonryState> = async (context, options, _onProgress) => {
+  // Require either rowHeight or columnWidth
+  options.flow === 'horizontal' ? requireState(context.state, 'rowHeight') : requireState(context.state, 'columnWidth');
+
+  // Rescale images to match rowHeight or columnWidth
+  const scaleOptions =
+    options.flow === 'horizontal'
+      ? { height: context.state.rowHeight, finalizePipeline: true }
+      : { width: context.state.columnWidth, finalizePipeline: true };
+  context.images = await scaleImages(context.images, scaleOptions);
+};
