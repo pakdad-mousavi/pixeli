dc467793b22a681fb9e3d6e123847addfe3e3379
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-08 18:25:33 +0800

    Remove unnecessary if guard.

--- src/core/merges/shared-steps/applyComposites.ts
+++ src/core/merges/shared-steps/applyComposites.ts
@@ -6,14 +6,6 @@ import { requireContextProp } from '../../pipeline/guards.js';
 export const applyComposites: MergeStep<any, any> = async (context, _options, _onProgress) => {
   requireContextProp(context, 'canvas');
 
-  // Ensure canvas exists
-  if (!context.canvas) {
-    throw new MergeError(MESSAGES.ERROR.INTERNAL.message, {
-      type: 'internal',
-      cause: 'cannot apply composites on undefined canvas',
-    });
-  }
-
   // Create final grid
   try {
     context.canvas = context.canvas.composite(context.composites);
