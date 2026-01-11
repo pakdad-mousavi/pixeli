7f7d5568fd3d365f88120a4b7b72784a3ba62fb3
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-11 18:43:19 +0800

    Create the collageSchema.

---
+++ src/core/schemas/collage.ts
@@ -0,0 +1,15 @@
+import z from 'zod';
+import { VALIDATORS } from '../../validators/index.js';
+
+export const collageSchema = z.strictObject({
+  shuffle: VALIDATORS.shuffle.default(false),
+  cornerRadius: VALIDATORS.cornerRadius.default(0),
+  gap: VALIDATORS.gap.default(50),
+  canvasColor: VALIDATORS.canvasColor.prefault('#fff'),
+  format: VALIDATORS.format.default('png'),
+  aspectRatio: VALIDATORS.aspectRatio.prefault('1:1'),
+  imageWidth: VALIDATORS.imageWidth.optional(),
+  columns: VALIDATORS.columns.default(4),
+  overlapPercentage: VALIDATORS.overlapPercentage.default(10),
+  rotationDegree: VALIDATORS.overlapPercentage.default(13),
+});
