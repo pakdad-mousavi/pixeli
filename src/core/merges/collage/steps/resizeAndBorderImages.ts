82969b00412bb786e3b53dc8dd28d5f81aa67015
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-14 13:09:30 +0800

    Create the resizeAndBorderImages collage merge step.

---
+++ src/core/merges/collage/steps/resizeAndBorderImages.ts
@@ -0,0 +1,44 @@
+import type { MergeStep } from '../../../pipeline/mergePipeline.js';
+import type { CollageState } from '../index.js';
+
+import { requireNonEmptyArray, requireState } from '../../../pipeline/guards.js';
+
+import { addImageBorder } from '../../../utils/images/addImageBorder.js';
+import { randint } from '../../../utils/math/randint.js';
+import type { RGBA } from '../../../utils/colors/types.js';
+
+interface Options {
+  imageWidthVariance: number;
+  aspectRatio: number;
+  borderWidth: number;
+  borderColor: RGBA;
+}
+
+export const resizeAndBorderImages: MergeStep<Options, CollageState> = async (context, options, _onProgress) => {
+  requireState(context, 'imageWidth');
+  requireState(context, 'imageHeight');
+  requireNonEmptyArray(context.images, 'images');
+
+  for (let i = 0; i < context.images.length; i++) {
+    const varianceWidth = randint(-options.imageWidthVariance, options.imageWidthVariance);
+    const width = context.state.imageWidth + varianceWidth;
+
+    const varianceHeight = Math.floor(varianceWidth / options.aspectRatio);
+    const height = context.state.imageHeight + varianceHeight;
+
+    const resizedImage = context.images[i]!.resize({
+      width,
+      height,
+    });
+
+    const borderedImage = await addImageBorder(resizedImage, {
+      imageWidth: width,
+      imageHeight: height,
+      borderWidth: options.borderWidth,
+      borderHeight: options.borderWidth,
+      borderColor: options.borderColor,
+    });
+
+    context.images[i] = borderedImage;
+  }
+};
