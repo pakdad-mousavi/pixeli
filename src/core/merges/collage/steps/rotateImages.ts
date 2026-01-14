4d7e588feef1b22750a4efd11721e4055e1f2eae
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-14 13:09:37 +0800

    Create the rotateImages collage merge step.

---
+++ src/core/merges/collage/steps/rotateImages.ts
@@ -0,0 +1,19 @@
+import type { MergeStep } from '../../../pipeline/mergePipeline.js';
+import type { CollageState } from '../index.js';
+
+import { requireNonEmptyArray } from '../../../pipeline/guards.js';
+
+import { randint } from '../../../utils/math/randint.js';
+
+interface Options {
+  rotationDegree: number;
+}
+
+export const rotateImages: MergeStep<Options, CollageState> = async (context, options, _onProgress) => {
+  requireNonEmptyArray(context.images, 'images');
+
+  for (let i = 0; i < context.images.length; i++) {
+    const angle = randint(-options.rotationDegree, options.rotationDegree);
+    context.images[i] = context.images[i]!.rotate(angle, { background: { r: 0, g: 0, b: 0, alpha: 0 } });
+  }
+};
