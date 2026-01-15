78d2910fad0416904a32350c553d5c87c36c1fc5
Pakdad Mousavi <pakdad.mousavi@gmail.com>
2026-01-15 12:44:46 +0800

    Get the CLI version from package.json.

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -8,12 +8,14 @@ import masonryCommand from './commands/masonry/index.js';
 import templateCommand from './commands/template/index.js';
 import collageCommand from './commands/collage/index.js';
 
+import pkg from '../../package.json' with { type: 'json' };
+
 const program = new Command();
 
 program
   .name('pixeli')
   .description('A lightweight command-line tool for merging multiple images into customizable grid layouts.')
-  .version('1.0.0');
+  .version(pkg.version);
 
 program.addCommand(gridCommand);
 program.addCommand(masonryCommand);
