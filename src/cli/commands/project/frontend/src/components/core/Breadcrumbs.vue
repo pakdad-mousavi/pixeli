7b90fe65461cc5e76ab92b75e4a4291cd7c7208e
pakdad-mousavi <pakdad.mousavi@gmail.com>
2026-09-25 16:36:16 +0800

    Make path names uniform in breadcrumbs.

--- src/cli/commands/project/frontend/src/components/core/Breadcrumbs.vue
+++ src/cli/commands/project/frontend/src/components/core/Breadcrumbs.vue
@@ -14,11 +14,11 @@ defineProps<{
       <div v-if="crumbs.length >= 3" class="text-zinc-500 dark:text-mist-500 flex items-center gap-x-1">
         <span>...</span>
         <span>></span>
-        <span class="text-mist-800/60 dark:text-mist-400 line-clamp-1">{{ crumbs.slice(crumbs.length - 1)[0] }}</span>
+        <span class="text-mist-800/60 dark:text-mist-400 line-clamp-1">{{ "/" + crumbs.slice(crumbs.length - 1)[0] }}</span>
       </div>
 
       <div v-else class="flex items-center gap-x-1 w-full">
-        <div v-for="(crumb, index) in crumbs" class="flex items-center gap-x-1 text-zinc-500 dark:text-mist-500">
+        <div v-for="(crumb, index) in crumbs" :key="`${index}-${crumb}`" class="flex items-center gap-x-1 text-zinc-500 dark:text-mist-500">
           <span
             class="line-clamp-1"
             :class="{
@@ -26,14 +26,14 @@ defineProps<{
               hidden: compact && index !== crumbs.length - 1,
             }"
           >
-            {{ crumb }}
+            {{ index === 0 ? "./" : "/" }}{{ crumb }}
           </span>
           <span :class="{ hidden: compact || index === crumbs.length - 1 }">></span>
         </div>
       </div>
 
       <div v-if="crumbs.length === 0">
-        <span class="text-mist-800/60 dark:text-mist-400 font-mono">./</span>
+        <span class="text-mist-800/60 dark:text-mist-400">.</span>
       </div>
     </div>
   </div>
